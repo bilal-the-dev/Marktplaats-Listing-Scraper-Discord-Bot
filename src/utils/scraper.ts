8e4d43d6f8d2a7f115a7e8202daa7fe3bb4b34c7
bilal-the-dev <bilalfarooq6001@gmail.com>
2025-08-14 00:38:42 +0500

    change limit to 30

--- src/utils/scraper.ts
+++ src/utils/scraper.ts
@@ -43,7 +43,7 @@ export const scraperAndProcessListings = async (
 
     prevSearchText = monitor.searchText;
 
-    const path = `/lrp/api/search?attributesById[]=0&attributesByKey[]=offeredSince%3AVandaag&limit=100&sortBy=SORT_INDEX&sortOrder=DECREASING&offset=0&query=${monitor.searchText}&searchInTitleAndDescription=true&viewOptions=list-view`;
+    const path = `/lrp/api/search?attributesById[]=0&attributesByKey[]=offeredSince%3AVandaag&limit=30&sortBy=SORT_INDEX&sortOrder=DECREASING&offset=0&query=${monitor.searchText}&searchInTitleAndDescription=true&viewOptions=list-view`;
 
     const data = await customFetch<MarktplaatsListingsResponse>({ path });
 
